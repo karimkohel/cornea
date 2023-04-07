3f0d98b956f2d8c709cbddb9d363b7e3ece6f83f
karimkohel <kareemkohel@gmail.com>
2023-04-07 15:33:35 +0200

    removed unecessary constants

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import cv2
 from corneaReader import CorneaReader
 
-LEFT_EYE=[362,382,381,380,374,373,390,249,263,466,388,387,386,385,384,398]
-RIGHT_EYE=[33,7,163,144,145,153,154,155,133,173,157,158,159,160,161,246]
-LEFT_IRIS=[474,475,476,477]
-RIGHT_IRIS=[469,470,471,472]
-
 cap = cv2.VideoCapture(0)
 cornea = CorneaReader()
 
