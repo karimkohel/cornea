5c81a15e742c5cf873e55aafd54d89647247883d
karimkohel <kareemkohel@gmail.com>
2023-04-07 21:11:29 +0200

    now showing distance between eyes

--- corneaReader.py
+++ corneaReader.py
@@ -8,12 +8,16 @@ class CorneaReader():
     """
 
     LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
-    RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
+    RIGHT_EYE = [133, 33, 7, 163, 144, 145, 153, 154, 155, 173, 157, 158, 159, 160, 161, 246]
     LEFT_IRIS = [474, 475, 476, 477]
     RIGHT_IRIS = [469, 470, 471, 472]
 
     def __init__(self) -> None:
         """Start the facemesh solution and be ready to read eye values
+
+        Returns:
+        ---------
+        values that i think would be beneficial to the model: distance from the iris to all eye landmarks, distance from closest point to each eye (distance between eyes)
         """
         self.faceMesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
@@ -34,13 +38,18 @@ class CorneaReader():
 
             eyeLandmarks = np.concatenate((meshPoints[self.LEFT_EYE], meshPoints[self.RIGHT_EYE]))
 
-            distances = np.linalg.norm(meshPoints[self.LEFT_EYE] - leftCenter, axis=1)
+            leftIrisDistances = np.linalg.norm(meshPoints[self.LEFT_EYE] - leftCenter, axis=1)
+            rightIrisDistances = np.linalg.norm(meshPoints[self.RIGHT_EYE] - rightCenter, axis=1)
+            middleEyeDistance = np.linalg.norm(meshPoints[self.RIGHT_EYE[0]] - meshPoints[self.LEFT_EYE[0]])
 
             for eyePoint in eyeLandmarks:
                 cv2.circle(frame, eyePoint, 1, (255,0,0), 1)
 
             for eyePoint in meshPoints[self.LEFT_EYE]:
                 cv2.line(frame, eyePoint, leftCenter, (0, 255,255), 1)
+
+            cv2.line(frame, meshPoints[self.RIGHT_EYE[0]], meshPoints[self.LEFT_EYE[0]], (100, 100, 255), 1)
+            print(middleEyeDistance)
 
 
             cv2.circle(frame, leftCenter, 1, (0,255,0), 1)
