47f7cd009e766c303c6f09f1518da9b44eaa1f87
karimkohel <kareemkohel@gmail.com>
2023-04-08 03:33:33 +0200

    splitting data and better simpler model

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
-import pandas as pd
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 
 data = np.load('data/trial1.npy')
 
+x = data[:, :33]
+y = data[:, 33:]
+
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.1, random_state=30)
+
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(0,33)),
-    tf.keras.layers.Dense(20, activation='relu'),
-    tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(40, activation='relu', input_dim=33),
+    tf.keras.layers.Dense(10),
+    tf.keras.layers.Dense(2, activation='relu')
 ])
+
