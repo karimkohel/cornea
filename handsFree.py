95ff0ec0f3432bc7905747d4f59bf99b340fc3c0
karimkohel <kareemkohel@gmail.com>
2023-04-08 15:45:25 +0200

    smoothing the output gets more accurate

--- handsFree.py
+++ handsFree.py
@@ -3,15 +3,26 @@ import tensorflow as tf
 import pyautogui
 import time
 
-data = np.load('data/test1.npy')
+data = np.load('data/test2.npy')
 
-x = data[:, :33]
-y = data[:, 33:]
+x = data[60:, :33]
+y = data[60:, 33:]
 
 model = tf.keras.models.load_model('trial1Model.h5')
 
 
+# for i in range(start=0, stop=len(x), step=10):
+#     try:
+
+
+
+
 predictions = model.predict(x)
 
-for prediction in predictions:
-    pyautogui.moveTo(prediction[0], prediction[1])
+for i in range(0, len(predictions), 10):
+    try:
+        values = predictions[i:i+10]
+        coordinates = np.average(values, axis=0)
+        pyautogui.moveTo(coordinates[0], coordinates[1])
+    except Exception:
+        break
