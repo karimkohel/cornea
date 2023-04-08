53a616f269b7406c1e76ca0b5948419d9c6cc85a
karimkohel <kareemkohel@gmail.com>
2023-04-08 02:59:52 +0200

    started a casual lil model

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+
+data = np.load('data/trial1.npy')
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(0,33)),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(10)
+])
