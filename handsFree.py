19d2764d7870a0472ec10012f9ab5d36e786cda5
karimkohel <kareemkohel@gmail.com>
2023-04-09 14:15:56 +0200

    fixed a dumb mistake in taking input for test

--- handsFree.py
+++ handsFree.py
@@ -6,33 +6,32 @@ from corneaReader import CorneaReader
 
 cap = cv2.VideoCapture(0)
 cornea = CorneaReader()
-model = tf.keras.models.load_model('trial1Model.h5')
+model = tf.keras.models.load_model('models/bigModel.h5')
 
 while True:
 
-    ret, frame = cap.read()
 
     x = None
     i=0
-    if ret:
-        while i<5:
-            sample, frame = cornea.readEyes(frame)
-            if type(sample) == np.ndarray:
-                i = i + 1
-                if type(x) != np.ndarray:
-                    x = sample[:33]
-                else:
-                    x = np.vstack([x, sample[:33]])
+    while i<10:
+        ret, frame = cap.read()
+        sample, frame = cornea.readEyes(frame)
+        if type(sample) == np.ndarray:
+            i = i + 1
+            if type(x) != np.ndarray:
+                x = sample[:33]
+            else:
+                x = np.vstack([x, sample[:33]])
 
-        predictions = model.predict(x)
-        coordinates = np.average(predictions, axis=0)
-        print(coordinates)
-        pyautogui.moveTo(coordinates[0], coordinates[1])
+    predictions = model.predict(x)
+    coordinates = np.average(predictions, axis=0)
+    print(coordinates)
+    pyautogui.moveTo(coordinates[0], coordinates[1])
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    cv2.imshow("Frame", frame)
     
 
 cap.release()
