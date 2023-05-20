7646a5fe9ad6222da91cd3480526f3c7ad76254e
mohameosama <moandosama1163@gmail.com>
2023-05-20 22:06:32 +0300

    put the data in numpy array

--- classes/cornea.py
+++ classes/cornea.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import numpy as np
 import os, time
 import pyautogui
+from numpy import savez_compressed
 
 
 class CorneaReader():
@@ -136,9 +137,11 @@ class CorneaReader():
                 frames.append(resizedFrame)
                 mousePos[i] = file['mousePos']
             frames = np.array(frames)
-
+        
             self.__showFrameThenExit(frames[2], 10)
-
+            all_data = tuple(eyesMetrics , frames , mousePos)
+            all_data = np.array(all_data)
+            savez_compressed('all_data.npz', all_data)
             return (eyesMetrics, frames, mousePos)
         else:
             return self.__paddingRestOfImage(frame)
