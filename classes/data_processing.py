8b803db3478dfada40cfd2480cb84b1c22677019
karimkohel <kareemkohel@gmail.com>
2023-05-30 21:18:45 +0200

    prepping datapreprocessing for examining

--- classes/data_processing.py
+++ classes/data_processing.py
@@ -10,37 +10,28 @@ import os
 
 
 class CustomDataGen(tf.keras.utils.Sequence):
-    def __init__(self, batch_size: int = 10000):
-        
+    def __init__(self, dataDir: str, batch_size: int = 10000):
+
+        self.dataDir = dataDir
         self.batch_size = batch_size
+
+        with open(f"data/{self.dataDir}/sampleCount.txt", "r") as f:
+            self.n = int(f.read())
+
+    
+    def __get_input(self, sample: int):
+
+        file = np.load(f"data/{self.dataDir}/{sample}.npz")
+        eyesMetrics = file['eyesMetrics']
+        resizedFrame = self.__paddingRestOfImage(file['croppedFrame'])
         
-    def on_epoch_end(self):
-        if self.shuffle:
-            self.df = self.df.sample(frac=1).reset_index(drop=True)
+        return (eyesMetrics, resizedFrame, mousePos)
     
-    def __get_input(self, path, bbox, target_size):
-    
-        xmin, ymin, w, h = bbox['x'], bbox['y'], bbox['width'], bbox['height']
-
-        image = tf.keras.preprocessing.image.load_img(path)
-        image_arr = tf.keras.preprocessing.image.img_to_array(image)
-
-        image_arr = image_arr[ymin:ymin+h, xmin:xmin+w]
-        image_arr = tf.image.resize(image_arr,(target_size[0], target_size[1])).numpy()
-
-        return image_arr/255.
-    
-    def __get_output(self, label, num_classes):
-        return tf.keras.utils.to_categorical(label, num_classes=num_classes)
+    def __get_output(self, sample: int):
+        file = np.load(f"data/{self.dataDir}/{sample}.npz")
+        return file['mousePos']
     
     def __get_data(self, batches):
-        # Generates data containing batch_size samples
-
-        path_batch = batches[self.X_col['path']]
-        bbox_batch = batches[self.X_col['bbox']]
-        
-        name_batch = batches[self.y_col['name']]
-        type_batch = batches[self.y_col['type']]
 
         X_batch = np.asarray([self.__get_input(x, y, self.input_size) for x, y in zip(path_batch, bbox_batch)])
 
@@ -49,15 +40,30 @@ class CustomDataGen(tf.keras.utils.Sequence):
 
         return X_batch, tuple([y0_batch, y1_batch])
     
-    def __getitem__(self, index):
+    def __paddingRestOfImage(self, image: np.ndarray) -> np.ndarray:
+        # Get the current size of the image
+        current_size = image.shape[:2]
+
+        # Compute the amount of padding needed
+        padding_width = self.TARGET_IMG_SIZE[1] - current_size[1]
+        padding_height = self.TARGET_IMG_SIZE[0] - current_size[0]
         
-        batches = self.df[index * self.batch_size:(index + 1) * self.batch_size]
+        image = cv2.copyMakeBorder(image,
+                                top=0,
+                                bottom=padding_height,
+                                left=0,
+                                right=padding_width,
+                                borderType=cv2.BORDER_CONSTANT,
+                                value=(0, 0, 0))
+        return image
+    
+    def __getitem__(self, index):
+        print(index)
+        exit(0)
+        
+        batches = self.df[index: next self.batchsize]
         X, y = self.__get_data(batches)        
         return X, y
     
     def __len__(self):
         return self.n // self.batch_size
-
-
-
-z= DataGen.preProcess('ososHome')
