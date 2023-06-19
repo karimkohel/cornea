28a5845aecf433fbc990dad7d433051aea6b0d99
karimkohel <kareemkohel@gmail.com>
2023-06-19 16:52:47 +0300

    new handsfree script using new gaze class

--- handsFree.py
+++ handsFree.py
@@ -1,37 +1,23 @@
-import numpy as np
-import tensorflow as tf
-import pyautogui
 import cv2
-from classes.cornea import CorneaReader
-import time
+import pyautogui
+from classes.gazeTracker import GazeTracker
 
 cap = cv2.VideoCapture(0)
-cornea = CorneaReader()
-model = tf.keras.models.load_model('models/convModelTest3.h5')
+gazeTracker = GazeTracker('models/convModelTest3.h5')
 
-
-for i in range(50):
+for i in range(30):
     _, frame = cap.read()
 
 
 while True:
 
-
-    inputFrames = []
-    inputMetrics = []
-    i=0
-    while i<3:
-        i = i + 1
+    frames = []
+    for _ in range(3):
         ret, frame = cap.read()
-        (eyeMetrics, inputFrame), frame = cornea.readEyes(frame)
-        inputFrames.append(cornea.preProcessOnTheFly(frame))
-        inputMetrics.append(eyeMetrics)
+        frames.append(frame)
 
-    inputMetrics = np.array(inputMetrics)
-    inputFrames = np.array(inputFrames)
-    predictions = model.predict([inputFrames, inputMetrics])
-    coordinates = np.average(predictions, axis=0)
-    print(coordinates)
+    coordinates = gazeTracker.track_gaze(frames)
+
     pyautogui.moveTo(coordinates[0], coordinates[1])
 
     key = cv2.waitKey(1)
